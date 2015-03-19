34ea8f5f377322ba68e71c5f40536b6722c09ca9
StickOnGit <the87thbombfish@gmail.com>
2015-03-19 16:58:23 -0500

    le sphere encore

--- zsphere.py
+++ zsphere.py
@@ -44,42 +44,42 @@ class ZSphere(ZShape):
                 (9, 8, 1)
             ]
         
-        #for group in pt_groups:
-        #    lines = [(0, 1), (0, 2), (1, 2)]
-        #    f_pts = [pts[x] for x in group]
-        #    # f_pts = group
-        #    f_lines = [(pts[a], pts[b]) for a, b in lines]
-        #    self.set_face(f_pts, f_lines)
-        refined_group = []
-        for abc in pt_groups:
-            tiny_group = []
-            a, b, c = [self.pts[i] for i in abc]
-            # radius = minus(a, self.center)
-            aa = [x / 2 for x in plus(a, b)]
-            bb = [y / 2 for y in plus(b, c)]
-            cc = [z / 2 for z in plus(c, a)]
-            # these points will be too close; need to
-            # push them out to lie on the sphere
-            
-            for pt in (aa, bb, cc):
-                vec = normalize_vector(minus(pt, self.center))
-                new_pt = coords(self.center, [vec], d)[0]
-                if new_pt not in self.pts:
-                    self.pts.append(new_pt)
-                else:
-                    new_pt = self.pts[self.pts.index(new_pt)]
-                tiny_group.append(new_pt)
-            
-            refined_group.append(tiny_group)
-                    # pt_cache.append(pt)
-        
-                    
-        # print len(self.pts)
-        for group in refined_group:
+        for group in pt_groups:
             lines = [(0, 1), (0, 2), (1, 2)]
-            f_pts = group
+            f_pts = [pts[x] for x in group]
+            # f_pts = group
             f_lines = [(pts[a], pts[b]) for a, b in lines]
             self.set_face(f_pts, f_lines)
+        #refined_group = []
+        #for abc in pt_groups:
+        #    tiny_group = []
+        #    a, b, c = [self.pts[i] for i in abc]
+        #    # radius = minus(a, self.center)
+        #    aa = [x / 2 for x in plus(a, b)]
+        #    bb = [y / 2 for y in plus(b, c)]
+        #    cc = [z / 2 for z in plus(c, a)]
+        #    # these points will be too close; need to
+        #    # push them out to lie on the sphere
+        #    
+        #    for pt in (aa, bb, cc):
+        #        vec = normalize_vector(minus(pt, self.center))
+        #        new_pt = coords(self.center, [vec], d)[0]
+        #        if new_pt not in self.pts:
+        #            self.pts.append(new_pt)
+        #        else:
+        #            new_pt = self.pts[self.pts.index(new_pt)]
+        #        tiny_group.append(new_pt)
+        #    
+        #    refined_group.append(tiny_group)
+        #            # pt_cache.append(pt)
+        # 
+        #            
+        # print len(self.pts)
+        #for group in refined_group:
+        #    lines = [(0, 1), (0, 2), (1, 2)]
+        #    f_pts = group
+        #    f_lines = [(pts[a], pts[b]) for a, b in lines]
+        #    self.set_face(f_pts, f_lines)
         """
         #pts = []
         #for i in xrange(48):
